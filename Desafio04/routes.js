fe6a7a9a779dc512edbfe50e9e7985ff1057cd71
miroswd <altamirsantos93@hotmail.com>
2020-03-03 17:27:08 -0300

    Desafio 04 - create frontend

--- Desafio04/routes.js
+++ Desafio04/routes.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const routes = express.Router()
 
+// Routes 
+const teachers = require('./teachers')
+
 routes.get('/', function(req,res){
   return res.redirect('teachers')
 })
 
+// === Teachers === //
+
 routes.get('/teachers', function(req,res){
   return res.render('teachers/index')
 })
+
+routes.get('/teachers/create', function(req,res){
+  return res.render('teachers/create')
+})
+
+// POST
+
+routes.post('/teachers', teachers.post)
+
+
+// === Students === //
 
 routes.get('/students', function(req,res){
   return res.render('students/index')
