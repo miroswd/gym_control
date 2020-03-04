b943b619ae460a0da073b013836650a71f9a558f
miroswd <altamirsantos93@hotmail.com>
2020-03-04 17:11:44 -0300

    Estruturando dados

--- Desafio04/teachers.js
+++ Desafio04/teachers.js
@@ -1,3 +1,41 @@
-exports.post = function (req,res) {
-  return res.redirect('teachers')
+const fs = require('fs')
+const data = require('./data.json')
+
+
+exports.post = function(req,res) {
+  
+  // Validate
+  const keys = Object.keys(req.body)
+  for (key of keys){
+    if (req.body[key] == ""){
+      return res.send('Fill in all fields')
+    }
+  }
+
+  let {avatar_url, name, birth, education, learning, segment} = req.body;
+
+  birth = Date.parse(birth)
+
+  const id = Number(data.teachers.lenght + 1)
+  const created_at = Date.now()
+
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    education,
+    learning,
+    segment,
+    created_at
+  })
+
+
+  fs.writeFile('data.json', JSON.stringify(data,null,2), function(err){
+
+    if(err) console.log(err)
+
+    return res.redirect('/teachers')
+  })
+
 }
